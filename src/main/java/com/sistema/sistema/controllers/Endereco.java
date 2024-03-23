5f5e104d1d861dbea5c313cac6c101cabaf77eb1
rayanebarbosapet <rayanebarbosapet@gmail.com>
2024-03-23 13:56:48 -0300

    adicionado classe pessoa e endereco

---
+++ src/main/java/com/sistema/sistema/controllers/Endereco.java
@@ -0,0 +1,5 @@
+package com.sistema.sistema.controllers;
+
+public class Endereco {
+
+}
---
+++ src/main/java/com/sistema/sistema/controllers/Pessoa.java
@@ -0,0 +1,10 @@
+package com.sistema.sistema.controllers;
+
+public abstract class Pessoa {
+
+    String name;
+    int dataNascimento;
+    String cpf;
+    Endereco endereco;
+    String Contato;
+}
